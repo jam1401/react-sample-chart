db86597ed41551daf9547e8dedb3a3680b9f19fd
Jeff Morgan <jam1401@gmail.com>
2021-04-12 09:31:06 -0700

    working chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Line } from "react-chartjs-2";
+import "@taeuk-gang/chartjs-plugin-streaming";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+const data = {
+  datasets: [
+    {
+      label: "Respiration",
+      fill: false,
+      // lineTension: 0.5,
+      backgroundColor: "rgba(75,192,192,0.4)",
+      borderColor: "rgba(75,192,192,1)",
+      borderCapStyle: "butt",
+      borderDash: [],
+      borderWidth: 0.5,
+      borderDashOffset: 0.0,
+      borderJoinStyle: "miter",
+      pointBorderColor: "rgba(75,192,192,1)",
+      pointBackgroundColor: "#fff",
+      pointBorderWidth: 0.5,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(75,192,192,1)",
+      pointHoverBorderColor: "rgba(220,220,220,1)",
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+    },
+  ],
+};
+
+const options = {
+  plugins: {
+    streaming: {
+      // per-chart option
+      frameRate: 30,
+      duration: 20000,
+      refresh: 400,
+      delay: 400,
+      onRefresh: function (chart) {
+        chart.data.datasets.forEach(function (dataset) {
+          dataset.data.push({
+            x: Date.now(),
+
+            y: Math.random(),
+          });
+        });
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "realtime",
+        gridLines: {
+          color: "rgba(0, 0, 0, 0)",
+        },
+        ticks: {
+          display: false,
+        },
+
+        display: false,
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          color: "rgba(0, 0, 0, 0)",
+        },
+        ticks: {
+          display: false,
+        },
+
+        display: false,
+      },
+    ],
+  },
+};
+
+class App extends Component {
+  render() {
+    return <Line data={data} options={options} />;
+  }
 }
 
 export default App;
